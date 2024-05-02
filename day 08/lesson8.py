d46c76050f050d0a8de1940eb5cd887bccdec139
gabrielavqofashvili <157018843+gabrielavqofashvili@users.noreply.github.com>
2024-05-02 20:59:40 +0400

    Add files via upload

---
+++ day 08/lesson8.py
@@ -0,0 +1,4 @@
+age = int(input("enter your age: "))
+howmuchtravelinfuture = int(input("how much time do you wanna travel into the future? "))
+ageandfuture = age + howmuchtravelinfuture
+print(ageandfuture)
---
+++ day 24/classwork.py
@@ -0,0 +1,8 @@
+# პირველი ბილეთის ნომერი
+ticket_number = 1
+
+# სანამ ბილეთების რაოდენობა 0 ზე მეტია
+while ticket_number <= 5:
+    print("ბილეთი №", ticket_number, " გაყიდულია")
+    ticket_number += 1
+
